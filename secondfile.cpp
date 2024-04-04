e69a4c9c2c842ca0f04142089c48dff5bb77fa47
thien06012001 <thien06012001@gmail.com>
2024-04-04 22:55:29 +0700

    single file commit

---
+++ secondfile.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout << "Hello, World!" << endl;
+    cout << "Second branch" << endl;
+    cout << "git kraken" << endl;
+    cout << "update function" << endl;
+    return 0;
+}
